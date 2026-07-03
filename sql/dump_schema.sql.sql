10291fa2eff99f5d7c06c1b357f49e1b7dcf57fe
Paola Andrea Torres Serrano <panalyticsts@outlook.com>
2026-07-03 16:16:57 -0500

    Add files via upload

---
+++ sql/dump_schema.sql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE funnel_events(
+  lead_id INT,
+  date TEXT,
+  source TEXT,
+  segment TEXT,
+  stage,
+  month
+);
+
+CREATE TABLE user_table (user_id INTEGER,date TEXT,device TEXT,sex TEXT);
